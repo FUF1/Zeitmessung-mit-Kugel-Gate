4ea574dbe4dca537aacbe51aebdfa15df7fe7b23
FUF1 <69962736+FUF1@users.noreply.github.com>
2022-10-23 14:04:20 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,6 @@
-input.onPinPressed(TouchPin.P0, function () {
-    start = input.runningTime()
-})
 // Reset-Funktion
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.clearScreen()
-    start = 0
-    ende = 0
-})
-input.onPinPressed(TouchPin.P1, function () {
-    ende = input.runningTime()
-    basic.pause(100)
-    zeit = (ende - start) / 1000
-    basic.showNumber(zeit)
     start = 0
     ende = 0
 })
@@ -21,5 +10,15 @@ let start = 0
 start = 0
 ende = 0
 basic.forever(function () {
-	
+    while (input.pinIsPressed(TouchPin.P0)) {
+        start = input.runningTime()
+    }
+    while (input.pinIsPressed(TouchPin.P1)) {
+        ende = input.runningTime()
+        basic.pause(100)
+        zeit = (ende - start) / 1000
+        basic.showNumber(zeit)
+        start = 0
+        ende = 0
+    }
 })
