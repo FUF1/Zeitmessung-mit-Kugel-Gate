317db1cd949e4aadc613e5d591af62e2f241f771
FUF1 <69962736+FUF1@users.noreply.github.com>
2022-10-23 13:28:20 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,27 @@
 input.onPinPressed(TouchPin.P0, function () {
     start = input.runningTime()
+    if (input.pinIsPressed(TouchPin.P1)) {
+        ende = input.runningTime()
+        basic.pause(100)
+        zeit = (ende - start) / 1000
+        basic.showNumber(zeit)
+        _4digit.show(zeit)
+    }
 })
+// Reset-Funktion
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.clearScreen()
+    _4digit.clear()
     start = 0
     ende = 0
 })
-input.onPinPressed(TouchPin.P1, function () {
-    ende = input.runningTime()
-})
+let zeit = 0
+let _4digit: grove.TM1637 = null
 let ende = 0
 let start = 0
 start = 0
 ende = 0
+_4digit = grove.createDisplay(DigitalPin.P0, DigitalPin.P0)
 basic.forever(function () {
-    if (input.pinIsPressed(TouchPin.P1)) {
-        basic.pause(100)
-        basic.showNumber((ende - start) / 1000)
-    }
+	
 })
