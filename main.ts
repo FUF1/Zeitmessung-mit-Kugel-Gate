f3a0b719cb3456f91091d1a0b3d983b11829d2a9
FUF1 <69962736+FUF1@users.noreply.github.com>
2022-10-27 13:45:10 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,9 +11,11 @@ ende = 0
 basic.forever(function () {
     while (input.pinIsPressed(TouchPin.P0)) {
         start = input.runningTime()
+        music.playTone(262, music.beat(BeatFraction.Whole))
     }
     while (input.pinIsPressed(TouchPin.P1)) {
         ende = input.runningTime()
+        music.playTone(262, music.beat(BeatFraction.Whole))
         basic.pause(100)
         zeit = (ende - start) / 1000
         basic.showNumber(zeit)
